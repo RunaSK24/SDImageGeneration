5250376a81c47584d50ec0bf03928a9dd6e4b8d0
ZhuGuorong <2484101884@qq.com>
2023-12-15 01:22:40 +0800

    fetch sample

--- src/main/resources/static/js/LoginAndRegister.js
+++ src/main/resources/static/js/LoginAndRegister.js
@@ -12,6 +12,7 @@ function GoToReg() {
     login_box.classList.add('hidden'); // 隐藏注册
     register_box.classList.remove('hidden'); // 显示登录
 }
+
 // 去注册按钮点击事件
 register.addEventListener('click', GoToReg)
 
@@ -20,6 +21,7 @@ function GoToLog() {
     register_box.classList.add('hidden');
     login_box.classList.remove('hidden');
 }
+
 // 去登录按钮点击事件
 login.addEventListener('click', GoToLog)
 
@@ -40,16 +42,46 @@ function regAction() {
     register_box.classList.add('hidden');
     login_box.classList.remove('hidden');
 }
+
 reg.addEventListener('click', regAction)
 
 function logAction() {
     let usernameInput = document.querySelector("#usernameInput");
     let passwordInput = document.querySelector("#passwordInput");
 
-    if (usernameInput.valueMissing || passwordInput.valueMissing) {
+    if (usernameInput.validity.valueMissing || passwordInput.validity.valueMissing) {
         alert("用户名或密码不能为空");
-        return
+        return;
     }
-    window.location.href = "Main.html";
+
+    const data = {
+        userName: usernameInput.value,
+        passWord: passwordInput.value
+    }
+
+
+    //发送请求获取所有数据
+    fetch('http://localhost:80/users/validate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' //指定以json格式发送数据
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json()) //以json格式解码
+        .then(res => {
+            //接收到数据后的处理
+            if (res.code === 21001) {
+                console.log(res);
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch(error => {
+            //出现异常处理
+            alert(error);
+            alert("发生错误，无法连接到服务器");
+        });
 }
+
 log.addEventListener('click', logAction)
